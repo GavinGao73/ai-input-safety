88c7711e4c95d02597ade2599e6fdba593cffe57
GavinGao73 <13918180626@163.com>
2026-03-17 17:05:26 +0100

    Update raster.en.js

--- assets/raster.en.js
+++ assets/raster.en.js
@@ -16,7 +16,7 @@
     version: "r1",
 
     // 全局高度削减（像素），值越大黑条越矮。英文建议 5
-    globalHeightTrim: 35,        // 高度削减
+    globalHeightTrim: 30,        // 高度削减
     globalVerticalOffset: 30,     // 向下偏移更多，补偿上移
 
     limits: {
